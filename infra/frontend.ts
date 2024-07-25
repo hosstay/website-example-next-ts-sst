69b3ac1c27244b056fccf5d952da6943de4b5012
Taylor Hoss <hosstay@gmail.com>
2024-07-25 15:35:32 -0500

    Removed "frontend" .env and just passed relevant environment variables via sst.aws.Nextjs

--- infra/frontend.ts
+++ infra/frontend.ts
@@ -3,4 +3,11 @@
 export const site = new sst.aws.Nextjs("Frontend", {
   // link: [rds],
   path: "packages/frontend",
+  environment: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
 });
